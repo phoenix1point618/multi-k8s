846c55adaf29e6b75487f75099af898d8f285ec8
Amila Jayatilaka <amilajayatilaka@192-168-1-169.tpgi.com.au>
2019-08-14 22:31:19 +1000

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t phoenix1point618/multi-client:latest -t phoenix1point618/multi-client:$SHA -f .client/Dockerfile ./client
-docker build -t phoenix1point618/multi-server:latest -t phoenix1point618/multi-server:$SHA -f .server/Dockerfile ./server
-docker build -t phoenix1point618/multi-worker:latest -t phoenix1point618/multi-worker:$SHA -f .worker/Dockerfile ./worker
+docker build -t phoenix1point618/multi-client:latest -t phoenix1point618/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t phoenix1point618/multi-server:latest -t phoenix1point618/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t phoenix1point618/multi-worker:latest -t phoenix1point618/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push phoenix1point618/multi-client:latest
 docker push phoenix1point618/multi-server:latest
